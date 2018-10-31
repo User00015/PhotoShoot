809a8d07d23e12a73b70d72671d875ed62f0b042
wayne byard <user00015@gmail.com>
2018-10-31 01:35:52 -0400

    Added login/registration

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhotoGallery.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(PhotoGallery.Areas.Identity.IdentityHostingStartup))]
+namespace PhotoGallery.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PhotoGalleryIdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("PhotoGalleryIdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<PhotoGalleryIdentityDbContext>();
+            });
+        }
+    }
+}
