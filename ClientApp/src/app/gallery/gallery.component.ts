3526897cc98dc69f6c51307b9f54f1837ec738ee
Wayne Byard <Wayne.Byard@xpo.com>
2018-11-19 16:56:38 -0500

    Added spinnah's

--- ClientApp/src/app/gallery/gallery.component.ts
+++ ClientApp/src/app/gallery/gallery.component.ts
@@ -3,7 +3,7 @@ import { ImageService } from "../services/image.service";
 import { BehaviorSubject, Subject } from "rxjs";
 import { DomSanitizer } from "@angular/platform-browser"
 import { Observable } from 'rxjs';
-import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-gallery',
@@ -15,19 +15,23 @@ export class GalleryComponent implements OnInit {
   public images$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
   faAngleDown = faAngleDown;
+  faSpinner = faSpinner;
 
   currentPage: number = 0;
   sections: string[];
   currentSelection$: Subject<string> = new Subject<string>();
   currentSelection: string;
+  loading: boolean = false;
 
   constructor(private imageService: ImageService, private sanitizer: DomSanitizer) {
   }
 
   onScroll() {
+    this.loading = true;
     this.imageService.getGalleryImages(this.currentPage++, this.currentSelection).subscribe((data: any) => {
       let sanitized = data.map(p => this.sanitizer.bypassSecurityTrustUrl(p));
       this.images$.next([...this.images$.getValue(), ...sanitized]);
+      this.loading = false;
     });
 
   }
@@ -44,10 +48,12 @@ export class GalleryComponent implements OnInit {
     });
 
     this.currentSelection$.subscribe(selection => {
+      this.loading = true;
       this.imageService.getGalleryImages(this.currentPage, selection).subscribe((data: any) => {
         let sanitized = data.map(p => this.sanitizer.bypassSecurityTrustUrl(p));
         this.currentSelection = selection;
         this.images$.next(sanitized);
+        this.loading = true;
       });
 
     });
